821264efafc7e56a6cf2ea9a1206a83afd4bf827
varenyamwebsite-cpu <varenyamwebsite@gmail.com>
2026-07-13 15:24:21 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import cors from "cors"
 import V1Router from "./routes/v1/index.js";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.use(express.json());
 app.use(cors())
